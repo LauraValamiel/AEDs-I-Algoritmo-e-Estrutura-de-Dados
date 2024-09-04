7edb781d6e32aec8ab3ce6597417d364fc1afc82
Laura Valamiel <140740714+LauraValamiel@users.noreply.github.com>
2024-09-04 12:20:24 -0300

    Create Fila.h

---
+++ Pilha/Fila.h
@@ -0,0 +1,40 @@
+#ifndef FILA_H_INCLUDED
+#define FILA_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "util.h"
+
+typedef struct{
+    TCelula *frente;
+    TCelula *tras;
+    int tamanho;
+}TFila;
+
+void FFVazia(TFila *Fila);
+
+int FVazia(TFila Fila);
+
+void ExcluirF(TFila *Fila, TProduto *Item);
+
+void LiberarF(TFila *Fila);
+
+void Enfileirar(TProduto x, TFila *Fila);
+
+void Desenfileirar(TFila *Fila, TProduto *Item);
+
+void ImprimirF(TFila fila);
+
+int PesquisarFila(TFila *Fila, TProduto *x);
+
+int FilasIguais(TFila *Fila, TFila *Fila2);
+
+void Remover(TFila *Fila, int n);
+
+void Intersecao(TFila *Fila, TFila *Fila2, TFila *Fila3);
+
+void Diferenca(TFila *Fila, TFila *Fila2, TFila *Fila3);
+
+
+
+#endif // FILA_H_INCLUDED
